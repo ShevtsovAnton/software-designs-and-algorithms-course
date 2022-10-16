6ecf90967d4d835a8eafacc215248f5f83cb27fc
Anton <a0660576041@gmail.com>
2022-10-16 13:36:40 +0300

    task-1

--- docs/2-building-blocks-of-oop-part-1/hometask/src/Point.ts
+++ docs/2-building-blocks-of-oop-part-1/hometask/src/Point.ts
@@ -1,1 +1,39 @@
-export class Point {}
+export class Point {
+  private x: number;
+  private y: number;
+
+  private calculateDistanceTo(x: number, y: number): number {
+    const distance: number = Math.sqrt(
+      Math.pow(this.x - x, 2) + Math.pow(this.y - y, 2)
+    );
+
+    return parseFloat(distance.toFixed(2));
+  }
+
+  constructor();
+  constructor(x?: number, y?: number) {
+    this.x = x ?? 0;
+    this.y = y ?? 0;
+  }
+
+  toString(): string {
+    return `(${this.x}, ${this.y})`;
+  }
+
+  distance(): number;
+  distance(other: Point): number;
+  distance(x: number, y: number): number;
+
+  distance(other?: Point | number, y?: number): number {
+    if (typeof other === 'number' && y !== undefined) {
+      return this.calculateDistanceTo(other, y);
+    }
+    if (other === undefined && y === undefined) {
+      return this.calculateDistanceTo(0, 0);
+    } else {
+      const point = other as Point;
+      const { x, y } = point;
+      return this.calculateDistanceTo(x, y);
+    }
+  }
+}
--- docs/2-building-blocks-of-oop-part-1/hometask/src/Shape.ts
+++ docs/2-building-blocks-of-oop-part-1/hometask/src/Shape.ts
@@ -1,3 +1,46 @@
+import { Point } from './point';
+
 export abstract class Shape {
+  protected points: Point[];
+  protected color: string;
+  protected filled: boolean;
+
+  constructor(points: Point[]);
+  constructor(points: Point[], color: string, filled: boolean);
+
+  constructor(
+    points: Point[],
+    color: string = 'green',
+    filled: boolean = true
+  ) {
+    if (points.length < 3) {
+      throw new Error('At least 3 points are required.');
+    } else {
+      this.points = points;
+      this.color = color;
+      this.filled = filled;
+    }
+  }
+
+  toString(): string {
+    return `A Shape with color of ${this.color} and${
+      this.filled ? '' : ' not'
+    } filled. Points: ${this.points.join(', ')}.`;
+  }
+
+  getPerimeter(): number {
+    const distances = this.points.map((point, index, points) => {
+      let distance: number;
+      if (index === 0) {
+        distance = point.distance(points[points.length - 1]);
+      } else {
+        distance = point.distance(points[index - 1]);
+      }
+      return parseFloat(distance.toFixed(2));
+    });
+
+    return distances.reduce((prev, curr) => prev + curr);
+  }
+
   abstract getType(): string;
 }
