b4de5fd2e772093ee7dfb19d95cca2e592ce1629
Anton <a0660576041@gmail.com>
2022-10-18 19:47:34 +0300

    task-2 complete

--- docs/3-building-blocks-of-oop-part-2/hometask/src/Consumable.ts
+++ docs/3-building-blocks-of-oop-part-2/hometask/src/Consumable.ts
@@ -1,1 +1,39 @@
-// your code goes here
+export abstract class Consumable implements Item {
+  private consumed: boolean;
+  private spoiled: boolean;
+
+  constructor(name: string, value: number, weight: number, spoiled?: boolean) {
+    super(name, value, weight);
+
+    this.consumed = consumed;
+    this.spoiled = spoiled ?? false;
+  }
+
+  public use(): void {
+    if (!this.consumed && !this.spoiled) {
+      return this.eat();
+    }
+  }
+
+  public eat(): void {
+    if (this.isConsumed) {
+      return `There is nothing left of the ${this.getName()} to consume.`;
+    }
+    this.consumed = true;
+    return `You eat the ${this.getName}.${
+      this.spoiled ? ' You feel sick.' : ''
+    }`;
+  }
+
+  public isConsumed(): boolean {
+    return this.consumed;
+  }
+
+  public setConsumed(consumed: boolean): void {
+    this.consumed = consumed;
+  }
+
+  public toString(): string {
+    return `Id: ${this.getId()} || ${this.getName()} - Value: ${this.getValue()}, Weight: ${this.getWeight()}`;
+  }
+}
--- docs/3-building-blocks-of-oop-part-2/hometask/src/Weapon.ts
+++ docs/3-building-blocks-of-oop-part-2/hometask/src/Weapon.ts
@@ -1,1 +1,83 @@
-// your code goes here
+export abstract class Weapon implements Item {
+  private MODIFIER_CHANGE_RATE: number = 0.05;
+  private baseDamage: number;
+  private damageModifier: number;
+  private baseDurability: number;
+  private durabilityModifier: number;
+
+  constructor(
+    name: string,
+    baseDamage: number,
+    baseDurability: number,
+    value: number,
+    weight: number
+  ) {
+    super(name, value, weight);
+
+    this.baseDamage = baseDamage;
+    this.damageModifier = 0.05;
+    this.baseDurability = baseDurability;
+    this.durabilityModifier = 0.05;
+  }
+  public polish(): void;
+
+  public use(): string {
+    if (this.getDurability() <= 0) {
+      return `You can't use the ${this.getName()}, it is broken.`;
+    }
+    this.durabilityModifier -= Weapon.MODIFIER_CHANGE_RATE;
+
+    return `You use the ${this.getName()}, dealing ${this.getDamage()} points of damage.${
+      this.getDurability() <= 0 ? ` The ${this.getName()} breaks.` : ''
+    }`;
+  }
+
+  public toString(): string {
+    return `${this.getName()} − Value: ${this.getValue()}, Weight: ${this.getWeight()}, Damage: ${this.getDamage()}, Durability: ${
+      100 * this.getDurability()
+    }%`;
+  }
+
+  public getDamage(): number {
+    return this.baseDamage + this.damageModifier;
+  }
+
+  public getDurability(): number {
+    return this.baseDurability + this.durabilityModifier;
+  }
+
+  public setDurability(): void {
+    this.baseDurability = this.baseDurability + this.durabilityModifier;
+    if (this.baseDurability > 1) {
+      this.baseDurability = 1;
+    }
+  }
+
+  public getDamageModifier(): number {
+    return this.damageModifier;
+  }
+
+  public setDamageModifier(value: number): void {
+    this.damageModifier = value;
+  }
+
+  public getBaseDamage(): number {
+    return this.baseDamage;
+  }
+
+  public setBaseDamage(value: number): void {
+    this.baseDamage = value;
+  }
+
+  public getDurabilityModifier(): number {
+    return this.durabilityModifier;
+  }
+
+  public setDurabilityModifier(value: number): void {
+    this.durabilityModifier = value;
+  }
+
+  public getBaseDurability(): number {
+    return this.baseDurability;
+  }
+}
